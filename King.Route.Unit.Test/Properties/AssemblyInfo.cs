1e8c4d44e1cd275a59526faad1049db8d59aaed2
Jef King <jefkin@microsoft.com>
2015-01-24 07:43:32 -0800

    build out

---
+++ King.Route.Unit.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("King.Route.Unit.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("King.Route.Unit.Test")]
+[assembly: AssemblyCopyright("Copyright © Jef King 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5dcd369a-ff07-4424-8222-4e042d534636")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ALPHA
 ========
+
+Routing should only route models to methods; and be easily overloaded
