d210ae87167fc027b471e4a3bc23d2f1268626d2
Jef King <jefkin@microsoft.com>
2015-01-24 07:38:09 -0800

    Project

---
+++ King.Route/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("King.Route")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("King.Route")]
+[assembly: AssemblyCopyright("Copyright © Jef King 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("d143f6f1-78a7-4420-b229-31580dbd57cf")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
