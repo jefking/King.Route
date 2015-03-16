55c97d1b7eeaaaa203a4304d7ab5548eb7bcc77c
Jef King <jef@agilebusinesscloud.com>
2015-03-16 12:35:41 -0700

    Remove

--- King.Route.Demo.MVC/Startup.cs
+++ King.Route.Demo.MVC/Startup.cs
@@ -1,14 +1,9 @@
 ﻿using Microsoft.Owin;
-using Owin;
 
 [assembly: OwinStartupAttribute(typeof(King.Route.Demo.MVC.Startup))]
 namespace King.Route.Demo.MVC
 {
     public partial class Startup
     {
-        public void Configuration(IAppBuilder app)
-        {
-            ConfigureAuth(app);
-        }
     }
 }
