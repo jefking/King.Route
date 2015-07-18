4d0192e2f39be4dcdc2c58debd1d706a95f2b975
Jef King <jef@agilebusinesscloud.com>
2015-07-17 19:20:30 -0700

    demo

--- King.Route.Demo.MVC/Controllers/Api/CompanyController.cs
+++ King.Route.Demo.MVC/Controllers/Api/CompanyController.cs
@@ -10,12 +10,12 @@
 
         public IEnumerable<Company> Get([FromUri] Company model)
         {
-            return this.route.Get<IEnumerable<Company>>("CompanyStorage/Save", model);
+            return this.route.Get<IEnumerable<Company>>("CompanyStorage/Search", model);
         }
         
         public void Post([FromBody] Company model)
         {
-            this.route.Send("CompanyStorage/Search", model);
+            this.route.Send("CompanyStorage/Save", model);
         }
     }
 }
